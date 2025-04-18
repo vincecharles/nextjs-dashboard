e91e271989088ac2796fdf3717cef924853d1762
VinceCharles <vincecharles71@gmail.com>
2025-04-18 19:18:33 +0800

    commit

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -9,34 +9,26 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
 
-
+// Map of links to display in the side navigation.
+// Depending on the size of the application, this would be stored in a database.
 const links = [
+  { name: 'Home', href: '/dashboard', icon: HomeIcon },
   {
-    name: 'Dashboard',
-    href: '/dashboard',
-    icon: HomeIcon,
-  },
-  {
-    name: 'Users',
-    href: '/dashboard/users',
-    icon: UserGroupIcon,
-  },
-  {
-    name: 'Documents',
-    href: '/dashboard/documents',
+    name: 'Invoices',
+    href: '/dashboard/invoices',
     icon: DocumentDuplicateIcon,
   },
+  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
 ];
 
-
 export default function NavLinks() {
-    const pathname = usePathname();
-    return (
+  const pathname = usePathname();
+  return (
     <>
       {links.map((link) => {
         const LinkIcon = link.icon;
         return (
-          <Link
+          <a
             key={link.name}
             href={link.href}
             className={clsx(
@@ -48,7 +40,7 @@ export default function NavLinks() {
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
-          </Link>
+          </a>
         );
       })}
     </>
